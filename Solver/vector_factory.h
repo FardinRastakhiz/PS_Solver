45b080e2807c406c106d7d7ad4bbd59b6e1d5add
omidacoder <omiddavar@yahoo.com>
2023-09-27 16:30:16 +0330

    Changed the implementation of petsc sequential solver

--- Solver/vector_factory.h
+++ Solver/vector_factory.h
@@ -7,8 +7,8 @@ namespace ses {
 	void create_vector(int size, LocalType* values, VI_VEC& out_vec);
 	void fill_petsc_vector(int size, LocalType* values, PETSC_VEC& out_vec);
 	void create_petsc_vector(int size, PETSC_VEC& out_vec);
-	void create_petsc_vectors(int iterations, int size, std::vector<PETSC_VEC> out_vecs);
-	void fill_petsc_vectors(int iterations, int size, LocalType** values, std::vector<PETSC_VEC> out_vecs);
+	//void create_petsc_vectors(int iterations, int size, std::vector<PETSC_VEC> out_vecs);
+	//void fill_petsc_vectors(int iterations, int size, LocalType** values, std::vector<PETSC_VEC> out_vecs);
 }
 
 #define SES_VEC_FACTORY
