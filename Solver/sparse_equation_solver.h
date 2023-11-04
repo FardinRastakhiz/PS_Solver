da16e4435c9bd6d0cad7f5a9bdc8cefd13a3ba95
omidacoder <omiddavar@yahoo.com>
2023-11-04 20:50:10 +0330

    fortran interfaces created and tested

--- Solver/sparse_equation_solver.h
+++ Solver/sparse_equation_solver.h
@@ -4,11 +4,11 @@
 #define CXDLL_API __declspec(dllexport)
 
 // Solvers for Use-Cases
-extern "C" CXDLL_API void ses_solve_pressure_cpu(int num_rows, int num_cols, int num_non_zero, int* row_indices, int* col_indices, double* values, double* b, double* x, int iteration_count, double precision, int use_open_mp, int num_threads);
-extern "C" CXDLL_API void ses_solve_pressure_gpu(int num_rows, int num_cols, int num_non_zero, int* row_indices, int* col_indices, double* values, double* b, double* x,int target_lib, int iteration_count, double precision, int platform, int device);
-extern "C" CXDLL_API void ses_solve_begin_density_cpu(int num_rows, int num_cols, int num_non_zero, int* row_indices, int* col_indices, double* values, double* b, double* x, int iteration_count, double precision, int use_open_mp, int num_threads);
-extern "C" CXDLL_API void ses_solve_begin_density_gpu(int num_rows, int num_cols, int num_non_zero, int* row_indices, int* col_indices, double* values, double* b, double* x, int target_lib, int iteration_count, int precision, int platform, int device);
-extern "C" CXDLL_API void ses_solve_next(double* rhs, double* x, int iteration_count, int precision);
+extern "C" CXDLL_API int ses_solve_pressure_cpu(int num_rows, int num_non_zero, int* row_indices, int* col_indices, double* values, double* b, double* x, int iteration_count, double precision, int use_open_mp, int num_threads);
+extern "C" CXDLL_API int ses_solve_pressure_gpu(int num_rows, int num_non_zero, int* row_indices, int* col_indices, double* values, double* b, double* x,int target_lib, int iteration_count, double precision, int platform, int device);
+extern "C" CXDLL_API int ses_solve_begin_density_cpu(int num_rows, int num_non_zero, int* row_indices, int* col_indices, double* values, double* b, double* x, int iteration_count, double precision, int use_open_mp, int num_threads);
+extern "C" CXDLL_API int ses_solve_begin_density_gpu(int num_rows, int num_non_zero, int* row_indices, int* col_indices, double* values, double* b, double* x, int target_lib, int iteration_count, int precision, int platform, int device);
+extern "C" CXDLL_API int ses_solve_next(double* rhs, double* x, int iteration_count, int precision);
 
 
 
